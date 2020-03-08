3ad64b2929ae6ba3095c63ebb26c2bfa713bb8ed
Ken Yoshihara <yshrkn@gmail.com>
2020-03-08 23:49:42 +0900

    type array, number, string

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+export {}
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1 ,2 ,3];
+let string2: Array<string> = ['Tokyo', 'Osaka', 'Kyoto'];
+
+let strings: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+let nijigen: number[][] = [
+  [50, 100],
+  [150, 300]
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
---
+++ src/number-string.ts
@@ -0,0 +1,10 @@
+export {}
+
+let year: number = 1976;
+console.log(year)
+
+let age: number = 0x2b;
+console.log(age);
+
+let name: string = 'Ham';
+console.log(name);
