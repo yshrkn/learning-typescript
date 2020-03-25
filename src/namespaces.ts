559955780030fa2e0e01dfe8a322bdd68b01b293
Ken Yoshihara <yshrkn@gmail.com>
2020-03-25 23:41:25 +0900

    namespaces

---
+++ src/namespaces.ts
@@ -0,0 +1,32 @@
+export {};
+
+namespace Japanese {
+  export namespace Tokyo {
+    export class Person {
+      constructor(public name: string) {}
+    }
+  }
+  export namespace Osaka {
+    export class Person {
+      constructor(public name: string) {}
+    }
+  }
+}
+
+namespace English {
+  export class Person {
+    constructor(
+      public firstName: string,
+      public middleName: string,
+      public lastName: string
+    ) {}
+  }
+}
+
+const me = new Japanese.Tokyo.Person('Ken');
+console.log(me.name);
+const meOsaka = new Japanese.Osaka.Person('Ken(osaka)');
+console.log(meOsaka.name);
+
+const michael = new English.Person('Michael', 'Joseph', 'Jackson');
+console.log(michael);
