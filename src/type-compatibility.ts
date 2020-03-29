a5530d74492162422fd987ebe3d2d522754b2aa6
Ken Yoshihara <yshrkn@gmail.com>
2020-03-29 22:28:57 +0900

    type compatibility

---
+++ src/type-compatibility.ts
@@ -0,0 +1,38 @@
+export {};
+
+let fooCompatible: any;
+let barCompatible: string = 'TypeScript';
+
+console.log(typeof fooCompatible);
+
+fooCompatible = barCompatible;
+
+console.log(typeof fooCompatible);
+
+let fooInCompatible: string;
+let barInCompatible: number = 1;
+
+// fooInCompatible = barInCompatible;
+
+let fooString: string;
+let barString: string = 'string';
+fooString = barString;
+
+let fooStringLiteral: 'fooStringLiteral' = 'fooStringLiteral';
+fooString = fooStringLiteral;
+
+let fooNumber: number;
+let fooNumberLiteral: 1976 = 1976;
+fooNumber = fooNumberLiteral;
+
+interface Animal {
+  age: number;
+  name: string;
+}
+
+class Person {
+  constructor(public age: number, public name: string) {}
+}
+
+let me: Animal;
+me = new Person(43, 'Ken');
